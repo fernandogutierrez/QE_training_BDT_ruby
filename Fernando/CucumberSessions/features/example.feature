0d1b77f0e4404cabf62747a0a02d46dc352a3308
Admin <Admin>
2017-04-21 22:22:04 +0000

    Practice cucumber features

--- Fernando/CucumberSessions/features/example.feature
+++ Fernando/CucumberSessions/features/example.feature
@@ -2,7 +2,7 @@ Feature: Alternation Example
 
 Scenario: Spotify web app should display the Spotify Web Player page
 
-Given I am in Spotify Web Player sign in page 
+Given I am in Spotify Web Player Sig in page
 When I insert the user and password 
-And I click on (Sig|a)
+And I click on $'Log in with Facebook'
 Then I will be able to see the Spotify Web Player page
---
+++ Fernando/CucumberSessions/features/step_definitions/steps_practice_validations.rb
@@ -0,0 +1,23 @@
+Given(/^I am in log in page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I insert a zip code \$(\d+)$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I insert a username \$'(^[a-z]{1,}$)'$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I insert a Country \$'(^\D{1,}$)'$/) do 
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I press enter key$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I will be able to see the Home page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
