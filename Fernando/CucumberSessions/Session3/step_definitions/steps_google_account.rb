a26ec9a85234b85ecce3f9cc9cdb7536fe132b16
Fernando Gutierrez <fernandogutierrez279@gmail.com>
2017-04-22 12:48:00 -0400

    Tasks and practices for session 3

---
+++ Fernando/CucumberSessions/Session3/step_definitions/steps_google_account.rb
@@ -0,0 +1,55 @@
+Given(/^I am on Google Sig Up form$/) do 
+
+end
+
+When(/^I insert on First Name textbox: '([a-zA-Z_]+)'$/) do |first_name|
+
+end
+
+When(/^I insert on Last Name textbox: '([a-zA-Z_]+)'$/) do |last_name|
+  
+end
+
+When(/^I insert ([a-zA-Z_]+) on Choose your name textbox$/) do |username|
+ 
+end
+
+When(/^I insert ((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{8,}) on Create Password textbox$/) do |password|
+ 
+end
+
+When(/^I select (January|February|March|April|May|June|July|August|September|Octuber|November|December) on Month textbox$/) do |month|
+ 
+end
+
+When(/^I insert ([1-2][0-9]|3[0-1]{1,2}) on Day textbox$/) do |day|
+ 
+end
+
+When(/^I insert ((19|20)\d{2}) on Year textbox$/) do |prefix,year|
+  
+end
+
+When(/^I select (male|female) on Gender dropdown menu$/) do |gender|
+ 
+end
+
+When(/^I insert ((6|7)\d{7}) on Mobile Phone textbox$/) do |mobile_arg1,mobile_arg2|
+ 
+end
+
+When(/^I insert ([a-zA-Z_0-9]+@(gmail|hotmail)+.com) on Your current email addres textbox$/) do |email_arg1,email_arg2|
+ 
+end
+
+When(/^I select ([A-Z][a-z]+) on Location dropdown menu$/) do |location|
+ 
+end
+
+When(/^I press 'Next step' button$/) do
+
+end
+
+Then(/^I am able to see a Confirmation Page$/) do
+ 
+end
