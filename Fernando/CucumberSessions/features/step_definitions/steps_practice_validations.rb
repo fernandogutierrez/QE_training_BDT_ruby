03f247c8ed130456f51b55469ef4303c44024a1e
Fernando Gutierrez <fernandogutierrez279@gmail.com>
2017-04-22 07:41:21 -0400

    Fixed practice about validations

--- Fernando/CucumberSessions/features/step_definitions/steps_practice_validations.rb
+++ Fernando/CucumberSessions/features/step_definitions/steps_practice_validations.rb
@@ -2,22 +2,22 @@ Given(/^I am in log in page$/) do
   puts "I am login"
 end
 
-When(/^I insert a zip code "(\d+)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I insert a zip code '(\d+)'$/) do |zip|
+  puts "I insert a zip code"
 end
 
-When(/^I insert a username '([a-z]+)'$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I insert a username '([a-z]+)'$/) do |username|
+  puts "I insert a username"
 end
 
-When(/^I insert a Country (\D+)$/) do 
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I insert a Country (\D+)$/) do |country|
+  puts "I insert a Country"
 end
 
 When(/^I press enter key$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  puts "I press enter key"
 end
 
 Then(/^I will be able to see the Home page$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  puts "I will be able tosee the home"
 end
